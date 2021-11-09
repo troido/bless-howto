4a398c2a035283795794ace2e1e2f50e6315d7d7
giorgigusakov <giorgi@troido.de>
2021-11-09 16:28:35 +0100

    Updated find_snapshot.sh script

--- scripts/find_snapshot.sh
+++ scripts/find_snapshot.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "Finding SNAPSHOT libraries"
-PROJECT_GRADLE="$(egrep -co '.*snapshot' build.gradle)"
-APP_MODULE_GRADLE="$(egrep -co '.*snapshot' app/build.gradle)"
-if [ $PROJECT_GRADLE -eq 0 ] && [$APP_MODULE_GRADLE -eq 0]; then
+echo "Finding SNAPSHOT libraries..."
+# Find word SNAPSHOT(case insensitive) in gradle files and save the output into the variable.
+PROJECT_GRADLE="$(egrep -oi '.*snapshot' build.gradle app/build.gradle)"
+# If the variable is empty then It's Ok, otherwise print the output and exit with error
+if [ -z "$PROJECT_GRADLE" ]; then
   echo Snapshot versions have not been found. It\'s OK
 else
+  echo "Detected Snapshots: $PROJECT_GRADLE"
   echo Found Snapshots in the app. You should not use snapshot version in this app
   exit 1
 fi
